828062aa9528db93506bd4614518cc4c67723afc
imshanadan <advikraj200@gmail.com>
2025-01-30 20:26:23 +0530

    Update anaysa.py

--- anaysa.py
+++ anaysa.py
@@ -1,158 +1,170 @@
-import os,requests,time
-import requests,os,sys
-from concurrent.futures import ThreadPoolExecutor as ThreadPool  
+# -*- coding:utf8 -*-
+:'######::'########:'########:'########:::::'###::::'########:
+'##... ##: ##.....:: ##.....:: ##.... ##:::'## ##:::... ##..::
+ ##:::..:: ##::::::: ##::::::: ##:::: ##::'##:. ##::::: ##::::
+. ######:: ######::: ######::: ########::'##:::. ##:::: ##::::
+:..... ##: ##...:::: ##...:::: ##.. ##::: #########:::: ##::::
+'##::: ##: ##::::::: ##::::::: ##::. ##:: ##.... ##:::: ##::::
+. ######:: ########: ########: ##:::. ##: ##:::: ##:::: ##::::
+:......:::........::........::..:::::..::..:::::..:::::..:::::
+# Supports python2 & python3
+# CREDIT   :  TRICKER S33R9T BRAND 
+# Author : S33R9T BRAND 
+# Date   : Sun Jul 19 00:19:27 2021
 
-try:
-	import requests
-except ModuleNotFoundError:
-	os.system('pip insta requests')
-	import requests
-import requests,os,sys,random
-try:
-	import requests
-except ModuleNotFoundError:
-	os.system("pip install requests")
-	import requests
+# Import Modules
+import os
+import sys
+import zlib
+import time
+import base64
+import marshal
+import py_compile
 
-b="\033[1;34m"#----------𝗯𝗹𝘂𝗲
-bl="\033[1;30m"#--------𝗯𝗹𝗮𝗰𝗸
-c="\033[1;36m"#----------𝗰𝘆𝗮𝗻
-g="\033[1;32m"#----------𝗴𝗿𝗲𝗲𝗻
-p="\033[1;35m"#----------𝗽𝘂𝗿𝗽𝗹𝗲
-r="\033[1;31m"#----------𝗿𝗲𝗱
-y="\033[1;33m"#----------𝘆𝗲𝗹𝗹𝗼𝘄
-w="\033[1;37m"#----------𝘄𝗵𝗶𝘁𝗲 {𝗲𝗻𝗱}
-S = '\033[1;37m';A = '\x1b[38;5;208m';R = '\x1b[38;5;46m';F = '\x1b[38;5;48m';Z = '\033[1;33m';A = '\x1b[1;97m';R = '\x1b[38;5;196m';Y = '\033[1;33m';G = '\x1b[38;5;48m';h = '\x1b[38;5;48m';B = '\x1b[38;5;8m';G1 = '\x1b[38;5;46m';G2 = '\x1b[38;5;47m';G3 = '\x1b[38;5;48m';G4 = '\x1b[38;5;49m';G5 = '\x1b[38;5;50m';X = '\33[1;34m';X1 = '\x1b[38;5;14m';X2 = '\x1b[38;5;123m';X3 = '\x1b[38;5;122m';X4 = '\x1b[38;5;86m';X5 = '\x1b[38;5;121m';S = '\x1b[1;96m';W = '\x1b[38;5;196m';hh = '\033[34;1m'
-import requests,os,sys
-from concurrent.futures import ThreadPoolExecutor as ThreadPool
+# Select raw_input() or input()
+if sys.version_info[0]==2:
+    _input = "raw_input('%s')"
+elif sys.version_info[0]==3:
+    _input = "input('%s')"
+else:
+    sys.exit("\n Your Python Version is not Supported!")
 
-try:
-    import requests
-except:
-    os.system("pip install requests")
-    import requests 
+# Encoding
+zlb = lambda in_ : zlib.compress(in_)
+b16 = lambda in_ : base64.b16encode(in_)
+b32 = lambda in_ : base64.b32encode(in_)
+b64 = lambda in_ : base64.b64encode(in_)
+mar = lambda in_ : marshal.dumps(compile(in_,'<x>','exec'))
+note = "\x23\x20\x4f\x62\x66\x75\x73\x63\x61\x74\x65\x64\x20\x77\x69\x74\x68\x20\x50\x79\x4f\x62\x66\x75\x73\x63\x61\x74\x65\x0a\x23\x20\x68\x74\x74\x70\x73\x3a\x2f\x2f\x77\x77\x77\x2e\x67\x69\x74\x68\x75\x62\x2e\x63\x6f\x6d\x2f\x68\x74\x72\x2d\x74\x65\x63\x68\x0a\x23\x20\x54\x69\x6d\x65\x20\x3a\x20%s\n\x23\x20\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x2d\x0a" % time.ctime()
 
-  
+def banner(): # Program Banner
+    print(' ╔═════════════════════════════════╗\n ║          PyObfuscate            ║\n ║  Simple Python Code Obfuscator  ║\n ║  Author : S33R9T BRAND         ║\n ║  Github : TRICKER S33R9T BRAND║\n ╚═════════════════════════════════╝\n')
 
-os.system('clear')
-name = input(f"{R}[{G1}+{R}]{G1} ENTER YOUR NAME : ").upper()
-logo = f"""
-{G1}
- 
-┏━━━┳━━━┳━━━┳━━━┳━━━┳━━━━┓
-┃┏━┓┃┏━━┫┏━┓┃┏━┓┃┏━┓┃┏┓┏┓┃
-┃┗━━┫┗━━┫┗━┛┃┃╋┃┃┃╋┃┣┛┃┃┗┛
-┗━━┓┃┏━━┫┏┓┏┫┗━┛┃┗━┛┃╋┃┃
-┃┗━┛┃┗━━┫┃┃┗┫┏━┓┃┏━┓┃╋┃┃
-┗━━━┻━━━┻┛┗━┻┛╋┗┻┛╋┗┛╋┗┛  
-                              
-                                                         
-{R}[{G1}+{R}]{G1}Decode{G1}━{R}>{G1}Marshal.Zlib.Decompress.Base64.B64decode
-{R}[{G1}+{R}]{G1}Yourname{G1}━{R}>{G1}{name}
-"""
-def main_dec():
-	os.system('clear')
-	print(logo)
-	try:
-		open("/data/data/com.termux/files/usr/bin/pycdc")
-		open("/data/data/com.termux/files/usr/lib/python3.11/minopyc.py", "r").read()
-		open("/data/data/com.termux/files/usr/bin/pycdas")
-	except:
-		os.system("curl -O https://raw.githubusercontent.com/i4mMino/pycdc/main/pycdc")
-		os.system("curl -O https://raw.githubusercontent.com/i4mMino/pycdc/main/pycdas")
-		os.system("curl -O https://raw.githubusercontent.com/i4mMino/pycdc/main/minopyc.py")
-		os.system("mv pycdc /data/data/com.termux/files/usr/bin/")
-		os.system("mv pycdas /data/data/com.termux/files/usr/bin/")
-		os.system("mv minopyc.py /data/data/com.termux/files/usr/lib/python3.11/")
-		os.system("chmod 777 /data/data/com.termux/files/usr/lib/python3.11/minopyc.py")
-		os.system("chmod 777 /data/data/com.termux/files/usr/bin/pycdc")
-		os.system("chmod 777 /data/data/com.termux/files/usr/bin/pycdas")
-	try:
-		file=input(f"{R}[{G1}+{R}]{G1} INPUT YOUR FILE : ")
-		open(file)
-		os.system(f"cp {file} .b.py")
-	except:
-		exit('Nawaka Hallaya')
-	try:
-		open(file).read()
-	except:
-		os.system(f"pycdc .b.py > .a.py")
-		files = open(".a.py", "r").read()
-		if "exec(str(chr" in files:
-			c= files.split(']')[0]+"]\nprint(''.join([chr(i) for i in _]))"
-			files = open(".a.py", "w").write(c)
-			os.system("python3 .a.py > .b.py")
-		else:
-			os.system("mv .a.py .b.py")
-			pass
-	print(f'{R}[{G1}+{R}]{G1} PLEASE WAIT I WILL TRYING TO DECODING')
-	while True:
-		file = open(".b.py", "r").read()
-		if "(__import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(b" in file:
-			filer = file.split("exec(")[1]
-			open(".b.py", "w").write("import minopyc,marshal\ncode =("+filer+"\nminopyc.dump_to_pyc(code, '.a.py')")
-			os.system("python3 .b.py;pycdc .a.py > .b.py")
-		elif "(__import__('marshal').loads(__import__('marshal').loads(__import__('marshal').loads(" in file:
-			filer = file.split("exec(")[1]
-			open(".b.py", "w").write("import minopyc,marshal\ncode =("+filer+"\nminopyc.dump_to_pyc(code, '.a.py')")
-			os.system("python3 .b.py;pycdc .a.py > .b.py")
-		elif "exec(_(" in file:
-			
-			c= file.split('exec(_(')[1]
-			l = ("import marshal,zlib,base64,minopyc\nx = (("+c+"\ny = x[:: -1]\nb = marshal.loads(zlib.decompress(base64.b64decode(y)))\nminopyc.dump_to_pyc(b,'.a.py') ")
-			open(".b.py","w").write(l)
-			os.system("python .b.py")
-			os.system("pycdc .a.py > .b.py")
-		elif "exec((_)(" in file:
-			c= file.split('exec((_)(')[1]
-			l = ("import marshal,zlib,base64,minopyc\nx = (("+c+"\ny = x[:: -1]\nb = marshal.loads(zlib.decompress(base64.b64decode(y)))\nminopyc.dump_to_pyc(b,'.a.py') ")
-			open(".b.py","w").write(l)
-			os.system("python .b.py")
-			os.system("pycdc .a.py > .b.py")
-		elif "exec(marshal.loads" in file:
-			filer = file.replace("exec(", "code=(")
-			open(".b.py", "w").write("import minopyc,marshal\n"+filer+"\nminopyc.dump_to_pyc(code, '.a.py')")
-			os.system("python3 .b.py;pycdc .a.py > .b.py")
-		elif "exec((lambda __," in file:
-			filer = file.replace("exec(", "print(")
-			open(".a.py", "w").write(filer)
-			os.system("python2 .a.py > .b.py")
-		else:
-			c= open(".b.py","r").read()
-			if c == '':
-				print(f'{R}[{G1}+{R}]{G1}THE TOOL CAN JUST DECODED DATA')
-				save=input(f"{R}[{G1}+{R}]{G1}ENTER PATH TO SAVE FROM : ")
-				os.system(f"pycdas .a.py > {save}")
-				os.system("rm .a.py;rm .b.py")
-			elif "WARNING: Decompyle incomplete" in c:
-				print(f'{R}[{G1}+{R}]{G1}THE TOOL CAN JUST DECODED DATA')
-				save=input(f"{R}[{G1}+{R}]{G1}ENTER PATH TO SAVE FROM : ")
-				os.system(f"pycdas .a.py > {save}")
-			else:
-				print(f'{R}[{G1}+{R}]{G1}THE TOOL DECODED')
-				save=input(f"{R}[{G1}+{R}]{G1}ENTER PATH TO SAVE FROM : ")
-				open(save, "w").write(c)
-				break
-			break
-	try:
-		open(".a.py")
-		os.system("rm .a.py")
-		try:
-			open(".b.py")
-			os.system("rm .b.py")
-		except:pass
-	except:pass
-	exit(f"{R}[{G1}+{R}]{G1}DECODE DONE🤑🤑Kalyan mitro")
-def contact_admin():
-	os.system('xdg-open https://facebook.com/groups/1036123894351028/')
+def menu(): # Program Menu
+    print("\x20\x5b\x30\x31\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x4d\x61\x72\x73\x68\x61\x6c\x0a\x20\x5b\x30\x32\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x5a\x6c\x69\x62\x0a\x20\x5b\x30\x33\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x42\x61\x73\x65\x31\x36\x0a\x20\x5b\x30\x34\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x42\x61\x73\x65\x33\x32\x0a\x20\x5b\x30\x35\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x42\x61\x73\x65\x36\x34\x0a\x20\x5b\x30\x36\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x5a\x6c\x69\x62\x2c\x42\x61\x73\x65\x31\x36\x0a\x20\x5b\x30\x37\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x5a\x6c\x69\x62\x2c\x42\x61\x73\x65\x33\x32\x0a\x20\x5b\x30\x38\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x5a\x6c\x69\x62\x2c\x42\x61\x73\x65\x36\x34\x0a\x20\x5b\x30\x39\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x4d\x61\x72\x73\x68\x61\x6c\x2c\x5a\x6c\x69\x62\x0a\x20\x5b\x31\x30\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x4d\x61\x72\x73\x68\x61\x6c\x2c\x42\x61\x73\x65\x31\x36\x0a\x20\x5b\x31\x31\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x4d\x61\x72\x73\x68\x61\x6c\x2c\x42\x61\x73\x65\x33\x32\x0a\x20\x5b\x31\x32\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x4d\x61\x72\x73\x68\x61\x6c\x2c\x42\x61\x73\x65\x36\x34\x0a\x20\x5b\x31\x33\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x4d\x61\x72\x73\x68\x61\x6c\x2c\x5a\x6c\x69\x62\x2c\x42\x31\x36\x0a\x20\x5b\x31\x34\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x4d\x61\x72\x73\x68\x61\x6c\x2c\x5a\x6c\x69\x62\x2c\x42\x33\x32\x0a\x20\x5b\x31\x35\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x4d\x61\x72\x73\x68\x61\x6c\x2c\x5a\x6c\x69\x62\x2c\x42\x36\x34\x0a\x20\x5b\x31\x36\x5d\x20\x53\x69\x6d\x70\x6c\x65\x20\x45\x6e\x63\x6f\x64\x65\x0a\x20\x5b\x31\x37\x5d\x20\x45\x78\x69\x74\n")
 
-app = ('''
+class FileSize: # Gets the File Size
+    def datas(self,z):
+        for x in ['Byte','KB','MB','GB']:
+            if z < 1024.0:
+                return "%3.1f %s" % (z,x)
+            z /= 1024.0
+    def __init__(self,path):
+        if os.path.isfile(path):
+            dts = os.stat(path).st_size
+            print(" [-] Encoded File Size : %s\n" % self.datas(dts))
+# FileSize('rec.py')
 
-     ┏━━━┳━━━┳━━━┳━━━┳━━━┳━━━━┓
-┃┏━┓┃┏━━┫┏━┓┃┏━┓┃┏━┓┃┏┓┏┓┃
-┃┗━━┫┗━━┫┗━┛┃┃╋┃┃┃╋┃┣┛┃┃┗┛
-┗━━┓┃┏━━┫┏┓┏┫┗━┛┃┗━┛┃╋┃┃
-┃┗━┛┃┗━━┫┃┃┗┫┏━┓┃┏━┓┃╋┃┃
-┗━━━┻━━━┻┛┗━┻┛╋┗┻┛╋┗┛╋┗┛  
-''')
-main_dec()
+# Encode Menu
+def Encode(option,data,output):
+    loop = int(eval(_input % " [-] Encode Count : "))
+    if option == 1:
+        xx = "mar(data.encode('utf8'))[::-1]"
+        heading = "_ = lambda __ : __import__('marshal').loads(__[::-1]);"
+    elif option == 2:
+        xx = "zlb(data.encode('utf8'))[::-1]"
+        heading = "_ = lambda __ : __import__('zlib').decompress(__[::-1]);"
+    elif option == 3:
+        xx = "b16(data.encode('utf8'))[::-1]"
+        heading = "_ = lambda __ : __import__('base64').b16decode(__[::-1]);"
+    elif option == 4:
+        xx = "b32(data.encode('utf8'))[::-1]"
+        heading = "_ = lambda __ : __import__('base64').b32decode(__[::-1]);"
+    elif option == 5:
+        xx = "b64(data.encode('utf8'))[::-1]"
+        heading = "_ = lambda __ : __import__('base64').b64decode(__[::-1]);"
+    elif option == 6:
+        xx = "b16(zlb(data.encode('utf8')))[::-1]"
+        heading = "_ = lambda __ : __import__('zlib').decompress(__import__('base64').b16decode(__[::-1]));"
+    elif option == 7:
+        xx = "b32(zlb(data.encode('utf8')))[::-1]"
+        heading = "_ = lambda __ : __import__('zlib').decompress(__import__('base64').b32decode(__[::-1]));"
+    elif option == 8:
+        xx = "b64(zlb(data.encode('utf8')))[::-1]"
+        heading = "_ = lambda __ : __import__('zlib').decompress(__import__('base64').b64decode(__[::-1]));"
+    elif option == 9:
+        xx = "zlb(mar(data.encode('utf8')))[::-1]"
+        heading = "_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__[::-1]));"
+    elif option == 10:
+        xx = "b16(mar(data.encode('utf8')))[::-1]"
+        heading = "_ = lambda __ : __import__('marshal').loads(__import__('base64').b16decode(__[::-1]));"
+    elif option == 11:
+        xx = "b32(mar(data.encode('utf8')))[::-1]"
+        heading = "_ = lambda __ : __import__('marshal').loads(__import__('base64').b32decode(__[::-1]));"
+    elif option == 12:
+        xx = "b64(mar(data.encode('utf8')))[::-1]"
+        heading = "_ = lambda __ : __import__('marshal').loads(__import__('base64').b64decode(__[::-1]));"
+    elif option == 13:
+        xx = "b16(zlb(mar(data.encode('utf8'))))[::-1]"
+        heading = "_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b16decode(__[::-1])));"
+    elif option == 14:
+        xx = "b32(zlb(mar(data.encode('utf8'))))[::-1]"
+        heading = "_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b32decode(__[::-1])));"
+    elif option == 15:
+        xx = "b64(zlb(mar(data.encode('utf8'))))[::-1]"
+        heading = "_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));"
+    else:
+        sys.exit("\n Invalid Option!")
+    
+    for x in range(loop):
+        try:
+            data = "exec((_)(%s))" % repr(eval(xx))
+        except TypeError as s:
+            sys.exit(" TypeError : " + str(s))
+    with open(output, 'w') as f:
+        f.write(note + heading + data)
+        f.close()
+
+# Special Encode
+def SEncode(data,output):
+    for x in range(5):
+        method = repr(b64(zlb(mar(data.encode('utf8'))))[::-1])
+        data = "exec(__import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(%s[::-1]))))" % method
+    z = []
+    for i in data:
+        z.append(ord(i))
+    sata = "_ = %s\nexec(''.join(chr(__) for __ in _))" % z
+    with open(output, 'w') as f:
+        f.write(note + "exec(str(chr(35)%s));" % '+chr(1)'*10000)
+        f.write(sata)
+        f.close()
+    py_compile.compile(output,output)
+
+# Main Menu
+def MainMenu():
+    try:
+        os.system('clear') # os.system('cls')
+        banner()
+        menu()
+        try:
+            option = int(eval(_input % " [-] Option : "))
+        except ValueError:
+            sys.exit("\n Invalid Option !")
+        
+        if option > 0 and option <= 17:
+            if option == 17:
+                sys.exit("\n Thanks For Using this Tool")
+            os.system('clear') # os.system('cls')
+            banner()
+        else:
+            sys.exit('\n Invalid Option !')
+        try:
+            file = eval(_input % " [-] File Name : ")
+            data = open(file).read()
+        except IOError:
+            sys.exit("\n File Not Found!")
+        
+        output = file.lower().replace('.py', '') + '_enc.py'
+        if option == 16:
+            SEncode(data,output)
+        else:
+            Encode(option,data,output)
+        print("\n [-] Successfully Encrypted %s" % file)
+        print(" [-] Saved as %s" % output)
+        FileSize(output)
+    except KeyboardInterrupt:
+        time.sleep(1)
+        sys.exit()
+
+if __name__ == "__main__":
+    MainMenu()
